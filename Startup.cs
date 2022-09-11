504457e6c7f4ce5871c0292f42df43dcc9e26d62
rpbrasil <rntprch@gmail.com>
2022-09-11 16:15:10 -0300

    ggl oauth test

--- Startup.cs
+++ Startup.cs
@@ -286,8 +286,8 @@ namespace DoctorCeo
                     {
                         // By default the Client will be redirect back to the URL that issued the challenge (/login?authtype=foo),
                         // send them to the home page instead (/).
-                        await context.ChallengeAsync(authType, new AuthenticationProperties() { RedirectUri = "/" });
-                        // await context.ChallengeAsync(authType, new AuthenticationProperties() { RedirectUri = "https://doctorceo.azurewebsites.net" });
+                        // await context.ChallengeAsync(authType, new AuthenticationProperties() { RedirectUri = "/" });
+                        await context.ChallengeAsync(authType, new AuthenticationProperties() { RedirectUri = "/privacy" });
                         return;
                     }
 
