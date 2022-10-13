3c87e425e51cbb0bfa4b96ada3f2f80e2ddf5d87
rpbrasil <rntprch@gmail.com>
2022-10-13 11:32:19 -0300

    test with email rowkey

--- Models/UserEntity.cs
+++ Models/UserEntity.cs
@@ -12,7 +12,7 @@ public class UserEntity : ITableEntity
     public string LastSigninDate { get; set; }
     //ITableEntity Members
     public virtual string PartitionKey { get => SigninProvider.ToString(); set => SigninProvider = value; }
-    public virtual string RowKey { get => LastSigninDate.ToString(); set => LastSigninDate = value; }
+    public virtual string RowKey { get => Email.ToString(); set => Email = value; }
     public DateTimeOffset? Timestamp { get; set; }
     public ETag ETag { get; set; }
 
